5e5be29c95a27b4093eb4413baca9ab3e3e15fdd
Dauda Hammed Oluwatoyin <dauda.hammed132@gmail.com>
2022-08-13 14:14:20 +0100

    Navbar Component

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.scss";
+import { HiMenu, HiMenuAlt4, HiX } from "react-icons/hi"
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
+   const [toggle, setToggle] = useState(true);
   return (
     <nav  className= "app__navbar">
       <div  className="app__navbar-logo">
@@ -9,13 +12,28 @@ const Navbar = () => {
       </div>
       
            <ul className="app__navbar-links">
-        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+        {['Home', 'services', 'Experience', 'Portfolio', 'Testimonials'].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
             <div />
             <a href={`#${item}`}>{item}</a>
           </li>
         ))}
       </ul>
+      <button>Contact</button>
+
+      <div className="app__navbar-menu">
+        
+            <HiMenuAlt4/>
+{toggle && ( <motion.div 
+            whileInView= {{ x: [300, 0]}} 
+            transition={{duration: 0.85, ease: "easeOut"}}
+            >
+                hammed
+            </motion.div>)}
+           
+
+
+      </div>
       
     </nav>
   )
